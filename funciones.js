e922a30802fe873d4fbfd1f04afb397e1e28dfac
carlos reyes <carlosarp2000@gmail.com>
2021-08-12 21:32:49 -0500

    funciones js

---
+++ funciones.js
@@ -0,0 +1,15 @@
+//  Funciones declarativas
+
+function miFuncion() {
+    return 3;
+}
+
+// Funcion de expresion
+
+// las variables pueden almacenar funciones
+
+var miFUncion = function(a, b) {
+    return a + b;
+}
+
+miFuncion();
